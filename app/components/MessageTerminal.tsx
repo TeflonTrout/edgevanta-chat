fd79ec32a99a72ec76fa887436ddb2cc6535ec39
TeflonTrout <jt@joy.world>
2024-03-13 13:22:00 -0400

    Updated styling and fixed loading bug on mobile devices

--- app/components/MessageTerminal.tsx
+++ app/components/MessageTerminal.tsx
@@ -41,15 +41,18 @@ const MessageTerminal = () => {
 
   return (
     <div className="flex flex-col w-full max-h-full sm:max-w-2/3 justify-start items-center bg-zinc-200 text-black">
-      <div className="flex flex-col w-2/3 md:w-5/6 max-h-96 min-h-96 border-4 rounded-lg z-10 scrollbar border-zinc-800">
+      <div
+        className="flex flex-col w-5/6 lg:w-1/2 border-4 rounded-lg z-10 scrollbar border-zinc-800"
+        style={{ maxHeight: "70vh" }}
+      >
         {isLoading ? (
-          <div className="flex flex-col w-full max-h-96 min-h-96 justify-center items-center border rounded border-zinc-800">
+          <div className="flex flex-col w-full h-svh justify-center items-center border rounded border-zinc-800">
             <Loading />
           </div>
         ) : (
           <div
             ref={listRef}
-            className="flex flex-col w-full justify-start py-6 px-4 pb-0 overflow-y-auto overflow-x-hidden"
+            className="flex flex-col w-full h-svh justify-start py-6 px-4 pb-0 overflow-y-auto overflow-x-hidden"
           >
             {messages?.length != 0 ? (
               messages?.map(({ _id, message, sender, _creationTime }) => (
@@ -62,19 +65,19 @@ const MessageTerminal = () => {
                 />
               ))
             ) : (
-              <div>
-                <h1>No current messages.</h1>
+              <div className="flex flex-col w-full h-svh justify-center overflow-y-auto overflow-x-hidden">
+                <h1 className="text-2xl text-center">No current messages.</h1>
               </div>
             )}
           </div>
         )}
         <form
-          className="flex flex-row w-2/3 md:w-5/6 justify-between items-center absolute bottom-6 gap-4 bg-zinc-200 text-black"
+          className="flex flex-row w-5/6 lg:w-1/2 justify-between items-center absolute bottom-6 gap-4 bg-zinc-200 text-black"
           action="submit"
           onSubmit={(e) => sendMessage(e)}
         >
           <input
-            className="flex p-2 w-5/6 rounded h-10 text-black outline-none"
+            className="flex p-2 w-2/3 rounded h-10 text-black outline-none"
             name="message"
             type="text"
             disabled={isLoading ? true : false}
@@ -83,7 +86,7 @@ const MessageTerminal = () => {
             onChange={(e) => setText(e.target.value)}
           ></input>
           <button
-            className="flex justify-center items-center h w-1/6 p-2 border bg-zinc-800 text-white border-zinc-800 rounded hover:bg-zinc-600 transition-colors duration-300"
+            className="flex justify-center items-center w-1/3 p-2 border bg-zinc-800 text-white border-zinc-800 rounded hover:bg-zinc-600 transition-colors duration-300"
             type="submit"
           >
             Send
